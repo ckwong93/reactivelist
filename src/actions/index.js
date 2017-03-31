c9c8b1cfbdbf31313e7cb03b9c3a0818bb736b95
Calvin Kwong <ckwong93@gmail.com>
2017-03-30 21:01:11 -0700

    add date picker

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
-export const addReminder = (text) => {
+export const addReminder = (text, dueDate) => {
   const action = {
     type: ADD_REMINDER,
-    text
+    text,
+    dueDate
     // text is es6 equivalent of text: text
   }
   console.log('action in addReminder', action);
